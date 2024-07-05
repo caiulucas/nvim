c752385d147f0898e5640ba8aaf026b2d9093a04
Caio Lucas <lienscarlet@gmail.com>
2024-07-05 19:55:08 -0300

    Update Nvim configs

---
+++ lua/plugins/colorizer.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "NvChad/nvim-colorizer.lua",
+    opts = {
+      user_default_options = {
+        tailwind = true,
+      }
+    },
+    config = function(_, opts)
+      require("colorizer").setup(opts)
+    end
+  }
+}
